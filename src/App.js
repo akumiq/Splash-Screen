420ea005503a5a8c7ba38a0030c172c2f413f3b0
Miqdad <kun.miqdad23@gmail.com>
2020-09-01 10:35:16 +0700

    set up react native extended stylesheet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Dimensions, View, Text } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
 
 const App = () => {
+  const entireScreenWidth = Dimensions.get('window').width
+  EStyleSheet.build({ $rem: entireScreenWidth / 320 })
+
   return (
     <View>
       <Text> App </Text>
